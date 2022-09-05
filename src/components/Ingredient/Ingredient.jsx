cda116cc9867a7ebd45dacc11e5aceb927ea15da
Nathalia Robbins-Cherry <nchomeldoe@gmail.com>
2022-09-05 13:26:46 +0100

    fetched all recipe data through to front end

--- src/components/Ingredient/Ingredient.jsx
+++ src/components/Ingredient/Ingredient.jsx
@@ -4,7 +4,6 @@ const Ingredient = ({ name, value, unit }) => {
   console.log("name value and unit", name, value, unit);
   return (
     <>
-      <h3>Ingredient</h3>
       <p>{name}</p>
       <p>{value}</p>
       <p>{unit}</p>
--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -3,58 +3,14 @@ import "./RecipeCard.scss";
 import Ingredient from "../Ingredient/Ingredient";
 
 const RecipeCard = ({ recipe }) => {
-  const [ingredientsAndQuantities, setIngredientsAndQuantities] = useState([]);
-  const { id, name, description, createdBy, cuisine, recipeElements } = recipe;
-
-  const getIngredient = async (id) => {
-    try {
-      let ingredientResponse = await fetch(
-        `http://localhost:8080/cookbook/ingredient/recipe-element?recipeElementId=${id}`,
-      );
-      if (!ingredientResponse.ok) {
-        throw new Error(ingredientResponse.status + " error with request");
-      }
-      let ingredientData = await ingredientResponse.json();
-      return ingredientData;
-    } catch (error) {
-      return error.message;
-    }
-  };
-
-  const getQuantity = async (id) => {
-    try {
-      let quantityResponse = await fetch(
-        `http://localhost:8080/cookbook/quantity/recipe-element?recipeElementId=${id}`,
-      );
-      if (!quantityResponse.ok) {
-        throw new Error(quantityResponse.status + " error with request");
-      }
-      let quantityData = await quantityResponse.json();
-      return quantityData;
-    } catch (error) {
-      return error.message;
-    }
-  };
-
-  const getAllIngredientsAndQuantities = () => {
-    const ingredientsAndQuantitiesArr = [];
-    recipeElements.forEach(async (recipeElement) => {
-      const ingredient = await getIngredient(recipeElement.id);
-      const quantity = await getQuantity(recipeElement.id);
-      ingredientsAndQuantitiesArr.push({
-        ingredientName: ingredient.name,
-        quantityValue: quantity.value,
-        quantityUnit: quantity.unit,
-      });
-    });
-    return ingredientsAndQuantitiesArr;
-  };
-
-  useEffect(() => {
-    setIngredientsAndQuantities(getAllIngredientsAndQuantities());
-  }, []);
-
-  console.log(ingredientsAndQuantities.length);
+  const {
+    id,
+    name,
+    description,
+    createdBy,
+    cuisine,
+    ingredientsAndQuantities,
+  } = recipe;
 
   return (
     <>
@@ -62,13 +18,14 @@ const RecipeCard = ({ recipe }) => {
       <p>{description}</p>
       <p>{cuisine}</p>
       <div>
+        <h4>Ingredients</h4>
         {ingredientsAndQuantities.length > 0 &&
           ingredientsAndQuantities.map((item, index) => (
             <Ingredient
               key={index}
-              name={item.ingredientName}
-              value={item.quantityValue}
-              unit={item.quantityUnit}
+              name={item.ingredient.name}
+              value={item.quantity.value}
+              unit={item.quantity.unit}
             />
           ))}
       </div>
