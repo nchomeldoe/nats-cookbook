1ce9b9f752b6c9dbfceb181b05d705299e561e9a
Nathalia Robbins-Cherry <nchomeldoe@gmail.com>
2022-09-06 08:15:38 +0100

    amended displayed units based on values

--- src/components/Ingredient/Ingredient.jsx
+++ src/components/Ingredient/Ingredient.jsx
@@ -1,11 +1,50 @@
 import "./Ingredient.scss";
 
 const Ingredient = ({ name, value, unit }) => {
-  console.log("name value and unit", name, value, unit);
+  if (value != 1) {
+    switch (unit) {
+      case "cup":
+        unit = "cups";
+        break;
+      case "can":
+        unit = "cans";
+        break;
+      case "tin":
+        unit = "tins";
+        break;
+      case "pinch":
+        unit = "pinches";
+        break;
+      case "drizzle":
+        unit = "drizzles";
+        break;
+      case "clove":
+        unit = "cloves";
+        break;
+      case "stick":
+        unit = "sticks";
+        break;
+      case "bunch":
+        unit = "bunches";
+        break;
+      case "packet":
+        unit = "packets";
+        break;
+      default:
+        unit = unit;
+        break;
+    }
+  }
+
+  if (unit == "item") {
+    unit = "";
+  }
+
   return (
     <>
       <p>
-        {name}: ({value} {unit})
+        {name}: ({Number(value)}
+        {unit.length > 0 && ` ${unit}`})
       </p>
     </>
   );
